db462fd0f2dd7375b4d71984d2505c677ea429fa
Marko <mali.coveculjak.rs@gmail.com>
2022-07-04 21:59:14 +0200

    Product microservice added

--- ProductApi/Dto/ProductDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProductApi.Dto
-{
-    public class ProductDto
-    {
-        public string Name { get; set; }
-        public double Price { get; set; }
-        public string Description { get; set; }
-    }
-}
